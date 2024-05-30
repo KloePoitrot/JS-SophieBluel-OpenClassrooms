1fcd0f702bdf2e8c88cd085c42628bf2314cfba1
unknown <KloePoitrot@hotmail.com>
2024-05-30 19:55:42 +0200

    Refactor modal script

---
+++ FrontEnd/assets/js/modal/modalContent.js
@@ -0,0 +1,39 @@
+function modalContent(workElements, catOption) {
+    return `
+    <div class="editModal">
+        <div class="navModal">
+            <i class="fas fa-x closeModal"></i>
+            <i class="fa-solid fa-arrow-left returnModal hidden"></i>
+        </div>
+        <article class="galleryEditContent">
+            <h2>Galerie Photo</h2>
+            <div id="editWorkGallery">
+                ` + workElements + `
+            </div>
+            <div class="divider"></div>
+            <input type="submit" name="addWork" value="Ajouter une photo">
+        </article>
+        
+        <article class="galleryAddContent hidden">
+                <h2>Ajout photo</h2>
+                <form>
+                    <div class="bluesection">
+                        <i class="fa-regular fa-image"></i> 
+                        <label for="file" class="uploadBtn">+ Ajouter photo</label>
+                        <input id="file" type="file">
+                        <p>jpg, png: 4mo max</p>
+                    </div>
+                    <label for="addTitle">Titre</label>
+                    <input type="text" id="addTitle">
+                    <label for="addCategorie">Catégorie</label>
+                    <select name="category" id="addCategory">
+                        <option value="none">--Choisissez une catégorie--</option>
+                        ` + catOption + `
+                    </select>
+                    <div class="divider"></div>
+                    <input class="greyBtn" type="submit" id="sent" value="Valider">
+                </form>
+            </article>
+    </div>
+    `
+}
--- FrontEnd/assets/js/modal/modalFunction.js
+++ FrontEnd/assets/js/modal/modalFunction.js
@@ -39,43 +39,8 @@ async function modalEdit(){
     // Create modal with work list
     let sectionEdit = document.createElement('div')
     sectionEdit.setAttribute("id", "editWork")
-    sectionEdit.innerHTML = `
-    <div class="editModal">
-        <div class="navModal">
-            <i class="fas fa-x closeModal"></i>
-            <i class="fa-solid fa-arrow-left returnModal hidden"></i>
-        </div>
-        <article class="galleryEditContent">
-            <h2>Galerie Photo</h2>
-            <div id="editWorkGallery">
-                ` + workElements + `
-            </div>
-            <div class="divider"></div>
-            <input type="submit" name="addWork" value="Ajouter une photo">
-        </article>
-        
-        <article class="galleryAddContent hidden">
-                <h2>Ajout photo</h2>
-                <form>
-                    <div class="bluesection">
-                        <i class="fa-regular fa-image"></i> 
-                        <label for="file" class="uploadBtn">+ Ajouter photo</label>
-                        <input id="file" type="file">
-                        <p>jpg, png: 4mo max</p>
-                    </div>
-                    <label for="addTitle">Titre</label>
-                    <input type="text" id="addTitle">
-                    <label for="addCategorie">Catégorie</label>
-                    <select name="category" id="addCategory">
-                        <option value="none">--Choisissez une catégorie--</option>
-                        ` + catOption + `
-                    </select>
-                    <div class="divider"></div>
-                    <input class="greyBtn" type="submit" id="sent" value="Valider">
-                </form>
-            </article>
-    </div>
-    `
+    sectionEdit.innerHTML = modalContent(workElements, catOption)
+
     document.body.appendChild(sectionEdit)
     // change to upload
     document.querySelector('input[name="addWork"]').addEventListener('click', () => {
